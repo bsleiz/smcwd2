f83877afb5acc95fbd58aebb54dffbd5a9820bc4
brian leisner <leisner_brian_s@student.smc.edu>
2020-06-05 22:25:29 -0700

    first phase final
    setting up pages

---
+++ Final Project/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+$('#submitted').on('click', function (){
+$('#formed').hide();
+
+
+    alert('Thank you for your grand gesture. We really appreciate your contribution.');
+document.write('Thank you for your grand gesture. We really appreciate your contribution.');
+});
+
+});
